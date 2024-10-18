978d5e2636cde392b71de0f1fb1d889ade70fba8
SamG1123 <mysteryman2330@gmail.com>
2024-10-19 00:39:18 +0530

    Pages
    Added different pages and made changes to existing ones

---
+++ data.js
@@ -0,0 +1,28 @@
+// data.js
+
+const users = {
+    students: [
+        { username: "student1", password: "password1" },
+        { username: "student2", password: "password2" }
+        // Add more student credentials here
+    ],
+    mentors: [
+        { username: "mentor1", password: "mentorpass1" },
+        { username: "mentor2", password: "mentorpass2" },
+        { username: "Prof. John Doe", password: "johnpass" },
+        { username: "Prof. Jane Smith", password: "janepass" }
+    ]
+};
+
+
+function validateLogin(username, password) {
+    const student = users.students.find(user => user.username === username && user.password === password);
+    const mentor = users.mentors.find(user => user.username === username && user.password === password);
+    
+    if (student) {
+        return { role: "student", user: student };
+    } else if (mentor) {
+        return { role: "mentor", user: mentor };
+    }
+    return null; 
+}
